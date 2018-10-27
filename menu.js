dac3cdb3b6c28bfa9d56a4fe93456d93fecc0d94
RCXcrafter <rcxcrafter@gmail.com>
2018-10-27 21:26:49 +0200

    remove .html from url

--- menu.js
+++ menu.js
@@ -1,13 +1,13 @@
 document.getElementById("navMenu").innerHTML =
-'<a href="index.html" class="button"><img src="icons/home.png">Homepage</a>'+
-'<a href="accounts.html" class="button"><img src="icons/rcxcrafter.png">Accounts</a>'+
+'<a href="https://rcxcrafter.github.io/" class="button"><img src="icons/home.png">Homepage</a>'+
+'<a href="https://rcxcrafter.github.io/accounts" class="button"><img src="icons/rcxcrafter.png">Accounts</a>'+
 '<p class="button">My Projects</p>'+
-'<a href="paileology.html" class="button"><img src="icons/paileology.png">Paileology</a>'+
-'<a href="compression.html" class="button"><img src="icons/compression.png">Additional Compression</a>'+
-'<a href="tweacon.html" class="button"><img src="icons/tweacon.png">Tweakers Construct</a>'+
-'<a href="eternalsingularity.html" class="button"><img src="icons/eternalsingularity.png">Eternal Singularity</a>'+
-'<a href="orechunks.html" class="button"><img src="icons/orechunks.png">AOBD Orechunks</a>'+
-'<a href="singularities.html" class="button"><img src="icons/singularity.png">AOBD Singularities</a>'+
-'<a href="lemontaco.html" class="button"><img src="icons/lemontaco.png">The Lemon Taco</a>'+
-'<a href="powerglove.html" class="button"><img src="icons/powerglove.png">Power Glove</a>'+
-'<a href="ragdoll.html" class="button"><img src="icons/ragdoll.png">Ragdoll Adventure</a>';
+'<a href="https://rcxcrafter.github.io/paileology" class="button"><img src="icons/paileology.png">Paileology</a>'+
+'<a href="https://rcxcrafter.github.io/compression" class="button"><img src="icons/compression.png">Additional Compression</a>'+
+'<a href="https://rcxcrafter.github.io/tweacon" class="button"><img src="icons/tweacon.png">Tweakers Construct</a>'+
+'<a href="https://rcxcrafter.github.io/eternalsingularity" class="button"><img src="icons/eternalsingularity.png">Eternal Singularity</a>'+
+'<a href="https://rcxcrafter.github.io/orechunks" class="button"><img src="icons/orechunks.png">AOBD Orechunks</a>'+
+'<a href="https://rcxcrafter.github.io/singularities" class="button"><img src="icons/singularity.png">AOBD Singularities</a>'+
+'<a href="https://rcxcrafter.github.io/lemontaco" class="button"><img src="icons/lemontaco.png">The Lemon Taco</a>'+
+'<a href="https://rcxcrafter.github.io/powerglove" class="button"><img src="icons/powerglove.png">Power Glove</a>'+
+'<a href="https://rcxcrafter.github.io/ragdoll" class="button"><img src="icons/ragdoll.png">Ragdoll Adventure</a>';
