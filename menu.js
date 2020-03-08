e9c4a3df35bb75e9c720aff02dac5b8f962ada38
RCXcrafter <rcxcrafter@gmail.com>
2020-03-08 16:31:24 +0100

    improve zooming in again

--- menu.js
+++ menu.js
@@ -2,12 +2,12 @@ document.getElementById("navMenu").innerHTML =
 '<a href="https://rcxcrafter.github.io/" class="button"><img src="icons/home.png">Homepage</a>'+
 '<a href="https://rcxcrafter.github.io/accounts" class="button"><img src="icons/rcxcrafter.png">Accounts</a>'+
 '<p class="button">My Projects</p>'+
-'<a href="https://rcxcrafter.github.io/paileology" class="button"><img src="icons/paileology.png">Paileology</a>'+
-'<a href="https://rcxcrafter.github.io/compression" class="button"><img src="icons/compression.png">Additional Compression</a>'+
-'<a href="https://rcxcrafter.github.io/tweacon" class="button"><img src="icons/tweacon.png">Tweakers Construct</a>'+
-'<a href="https://rcxcrafter.github.io/eternalsingularity" class="button"><img src="icons/eternalsingularity.png">Eternal Singularity</a>'+
-'<a href="https://rcxcrafter.github.io/orechunks" class="button"><img src="icons/orechunks.png">AOBD Orechunks</a>'+
-'<a href="https://rcxcrafter.github.io/singularities" class="button"><img src="icons/singularity.png">AOBD Singularities</a>'+
-'<a href="https://rcxcrafter.github.io/lemontaco" class="button"><img src="icons/lemontaco.png">The Lemon Taco</a>'+
-'<a href="https://rcxcrafter.github.io/powerglove" class="button"><img src="icons/powerglove.png">Power Glove</a>'+
-'<a href="https://rcxcrafter.github.io/ragdoll" class="button"><img src="icons/ragdoll.png">Ragdoll Adventure</a>';
+'<a href="https://rcxcrafter.github.io/paileology" class="button"><img src="icons/paileology.png" height="32" width="32">Paileology</a>'+
+'<a href="https://rcxcrafter.github.io/compression" class="button"><img src="icons/compression.png" height="32" width="32">Additional Compression</a>'+
+'<a href="https://rcxcrafter.github.io/tweacon" class="button"><img src="icons/tweacon.png" height="32" width="32">Tweakers Construct</a>'+
+'<a href="https://rcxcrafter.github.io/eternalsingularity" class="button"><img src="icons/eternalsingularity.png" height="32" width="32">Eternal Singularity</a>'+
+'<a href="https://rcxcrafter.github.io/orechunks" class="button"><img src="icons/orechunks.png" height="32" width="32">AOBD Orechunks</a>'+
+'<a href="https://rcxcrafter.github.io/singularities" class="button"><img src="icons/singularity.png" height="32" width="32">AOBD Singularities</a>'+
+'<a href="https://rcxcrafter.github.io/lemontaco" class="button"><img src="icons/lemontaco.png" height="32" width="32">The Lemon Taco</a>'+
+'<a href="https://rcxcrafter.github.io/powerglove" class="button"><img src="icons/powerglove.png" height="32" width="32">Power Glove</a>'+
+'<a href="https://rcxcrafter.github.io/ragdoll" class="button"><img src="icons/ragdoll.png" height="32" width="32">Ragdoll Adventure</a>';
